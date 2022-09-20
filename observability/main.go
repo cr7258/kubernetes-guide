9a8ce5fdc24655b54ccbcb14f426696ca1431e8d
Cheng, Seven <seven.cheng@sap.com>
2022-09-20 23:38:34 +0800

    jaeger provider

--- observability/main.go
+++ observability/main.go
@@ -3,23 +3,14 @@ package main
 import (
 	"context"
 	"go.opentelemetry.io/otel"
-	"log"
 	"observability/pkg/exporter"
-	"os"
 	"time"
 )
 
 const TraceName = "mytrace"
 
 func main() {
-	f, err := os.OpenFile("trace.txt", os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0600)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	defer f.Close()
-
-	tp := exporter.NewProvider(f)
+	tp := exporter.NewJaegerProvider()
 	ctx, span := otel.Tracer(TraceName).Start(context.Background(), "main")
 
 	time.Sleep(time.Second * 3)
@@ -27,4 +18,3 @@ func main() {
 	span.End()
 	tp.ForceFlush(ctx)
 }
-
